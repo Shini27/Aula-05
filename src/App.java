bfcbb0db1ac682b14d73607c25139ae3d4bca561
Shni <marcos_prado_tec@hotmail.com>
2024-03-20 12:13:39 -0300

    Commit data

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Fala mundão!!!");
+        System.out.println("Fala mundão!!! 20/02/2024");
     }
 }
